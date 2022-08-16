56ac9969e4b43a2bbc73aa2ac98a26af12dc41a7
IshmeetSingh06 <singhishmeet16@gmail.com>
2022-08-16 13:35:33 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.pinimg.com", "lh3.googleusercontent.com"],
+  },
   swcMinify: true,
-}
+};
 
 module.exports = nextConfig
